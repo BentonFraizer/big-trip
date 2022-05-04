9fe1db88cc1736a7dcc22d14bf5db04c1b8360eb
Yuri Lopukhin <FOL2203012@yandex.ru>
2022-05-04 17:21:08 +0300

    Доделано рандомное изменение иконки точки маршрута

--- src/view/event/event.tpl.js
+++ src/view/event/event.tpl.js
@@ -65,7 +65,7 @@ const createEventTemplate = (point) => {
     `<div class="event">
       <time class="event__date" datetime="${dayjs(dateFrom).format('YYYY-MM-DD')}">${humanizeDateMonthDay(dateFrom)}</time>
       <div class="event__type">
-        <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
       </div>
       <h3 class="event__title">${type} ${destination.name}</h3>
       <div class="event__schedule">
