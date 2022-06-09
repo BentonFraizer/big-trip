031c950ccfe0eccb860b0bbf568795d259c13695
Yuri Lopukhin <FOL2203012@yandex.ru>
2022-06-09 11:25:28 +0300

    Исправлена ошибка использования npm пакета he

--- src/views/edit_event_form/edit-event-form.tpl.js
+++ src/views/edit_event_form/edit-event-form.tpl.js
@@ -171,7 +171,7 @@ const createEditEventFormTemplate = (point, allOffers) => {
               <span class="visually-hidden">Price</span>
               €
             </label>
-            <input class="event__input  event__input--price" id="event-price-1" type="number" min="0" name="event-price" value="${he.encode(basePrice)}">
+            <input class="event__input event__input--price" id="event-price-1" type="number" min="0" name="event-price" value="${he.encode(String(basePrice))}">
           </div>
 
           <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
